34f075e8e3df15b26e7c8119d6b34df5c08ab9c0
sleepsim <91400544+sleepsim@users.noreply.github.com>
2023-12-05 01:44:57 -0800

    working details page as well

--- equipmentdetails.php
+++ equipmentdetails.php
@@ -4,28 +4,61 @@
     
 
     if(isset($_GET['itm_code']) && !empty($_GET['itm_code'])){
-        $query = "SELECT * 
+
+
+        $initialQuery = "SELECT * FROM equipment WHERE itm_code = ?";
+        $initStmt = $db->prepare($initialQuery);
+        $initStmt->bind_param('s', $_GET['itm_code']);
+        $initStmt->execute();
+        $initResult = $initStmt->get_result();
+        $initType = mysqli_fetch_assoc($initResult);
+
+        if($initType['type'] == 'Racket'){
+            $query = "SELECT * 
                   FROM equipment
                   JOIN racket ON equipment.itm_code = racket.itm_code
                   WHERE equipment.itm_code = ?";
 
-        $stmt = $db->prepare($query);
-        $stmt->bind_param('s', $_GET['itm_code']);
-        $stmt->execute();
+            $stmt = $db->prepare($query);
+            $stmt->bind_param('s', $_GET['itm_code']);
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            $productDetails = $result->fetch_assoc();
+
+            // Set variables for racket
+            $itemCode = $productDetails['itm_code'];
+            $manufacturer = $productDetails['manufacturer'];
+            $weight = $productDetails['weight'];
+            $balance = $productDetails['balance'];
+            $stiffness = $productDetails['stiffness'];
+            $img = $productDetails['img'];
+            $itemName = $productDetails['name'];
+        }else{
+            $query = "SELECT * 
+                  FROM equipment
+                  JOIN shoes ON equipment.itm_code = shoes.itm_code
+                  WHERE equipment.itm_code = ?";
+
+            $stmt = $db->prepare($query);
+            $stmt->bind_param('s', $_GET['itm_code']);
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            $productDetails = $result->fetch_assoc();
+
+            // Set variables for non-racket
+            $itemName = $productDetails['name'];
+            $manufacturer = $productDetails['manufacturer'];
+            $itemCode = $productDetails['itm_code'];
+            $img = $productDetails['img'];
+            $description = $productDetails['itm_desc'];
+        }
+
     }
 
-    $result = $stmt->get_result();
-    $productDetails = mysqli_fetch_assoc($result);
-
-    // Product information
-    $itemCode = $productDetails['itm_code'];
-    $manufacturer = $productDetails['manufacturer'];
-    $weight = $productDetails['weight'];
-    $balance = $productDetails['balance'];
-    $stiffness = $productDetails['stiffness'];
-    $img = $productDetails['img'];
-    $itemName = $productDetails['name'];
-
+    $initResult->free_result();
+    
 
 ?>
 
@@ -43,7 +76,7 @@
 
 <div class="container-fluid">
     <div class="row pl-5 pt-3">
-        <h6><a href="equipmentlist.php">All Equipment</a> / <a href=""><?php echo $manufacturer; ?></a> / <?php echo $productDetails['itm_code']; ?></h6>
+        <h6><a href="equipmentlist.php">All Equipment</a> / <a href="equipmentlist.php?type=all&manufacturer=<?php echo $manufacturer?>"><?php echo $manufacturer; ?></a> / <?php echo $productDetails['itm_code']; ?></h6>
     </div>
 
     <div class="row justify-content-center">
@@ -56,11 +89,16 @@
             <h2 class="mt-5"><?php echo $productDetails['name']; ?></h2>
             <h5 class="mb-3 mt-3">Specs</h5>
 
-            <p>Item Code: <?php echo $itemCode; ?></p>
-            <p>Manufacturer: <?php echo $manufacturer; ?></p>
-            <p>Weight: <?php echo $weight; ?></p>
-            <p>Balance: <?php echo $balance; ?></p>
-            <p>Stiffness: <?php echo $stiffness; ?></p>
+            <?php if ($productDetails['type'] == 'Racket'): ?>
+            <p>Item Code: <?php echo $productDetails['itm_code']; ?></p>
+            <p>Manufacturer: <?php echo $productDetails['manufacturer']; ?></p>
+            <p>Weight: <?php echo $productDetails['weight']; ?></p>
+            <p>Balance: <?php echo $productDetails['balance']; ?></p>
+            <p>Stiffness: <?php echo $productDetails['stiffness']; ?></p>
+            <?php else: ?>
+            <p>Item Code: <?php echo $productDetails['itm_code']; ?></p>
+            <p>Description: <?php echo $productDetails['itm_desc']; ?></p>
+            <?php endif; ?>
         </div>
     </div>
 
